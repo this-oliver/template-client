c9b8243570db963f72f8e14689bf7d9857e6957b
Oliver Manzi <olivermanzi.dev@gmail.com>
2023-05-10 17:12:27 +0200

    patch: adds initial nuxt config (v0.0.1)
    - installs pinia and vuetify modules
    - configures vuetify in /plugins
    - adds favicon and images for logo in /public
    - adds font-styling in /assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: [
+    '~/assets/styles/fonts.css'
+  ],
 
+  modules: [
+    '@pinia/nuxt',
+    '@invictus.codes/nuxt-vuetify'
+  ]
 })
