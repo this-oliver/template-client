fec759780f939e575f854b6885ab34040b804a11
Oliver Manzi <olivermanzi.dev@gmail.com>
2023-11-09 20:30:56 +0100

    (v0.6.2) patch: updates landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,60 @@
 <script setup lang="ts">
 import { useSidebarStore } from '~/stores/sidebar-store';
+import { useNotification } from '~/composables/useNotification';
+import type { ActionItem } from '~/components/base/BaseCard.vue';
 
-const sidebarStore = useSidebarStore();
 const runtimeConfig = useRuntimeConfig()
+const sidebarStore = useSidebarStore();
+const { notify } = useNotification();
+
+const features: ActionItem[] = [
+  {
+    label: 'Sidebar',
+    description: 'Toggle the sidebar',
+    action: () => sidebarStore.toggle()
+  },
+  {
+    label: 'Authentication',
+    description: 'Basic authentication',
+    to: '/auth/login',
+  },
+  {
+    label: 'Forms',
+    description: 'Form examples',
+    to: '/form',
+  },
+  {
+    label: 'Notifications',
+    description: 'Trigger notifications',
+    action: () => notify('Hello', 'This is a notification')
+  },
+]
 
 </script>
 
 <template>
-  <BasePage title="Welcome!" class="text-center">
-    <h2>This is a Nuxt template</h2>
-
-    <v-row class="mt-2" justify="center">
+  <base-page>
+    <v-row class="mt-2">
       <v-col cols="11" md="6">
-        <BaseCard>
-          <v-card-title>Store Demo</v-card-title>
-          <v-card-subtitle>See how the store works</v-card-subtitle>
-
-          <v-card-text>
-            Theme {{ sidebarStore.visible ? 'Visible' : 'Hidden' }}
-          </v-card-text>
-          
-          <BaseBtn tonal @click="sidebarStore.toggle()">
-            Toggle Sidebar
-          </BaseBtn>
-        </BaseCard>
+        <base-card>
+          <v-card-title>Env Support</v-card-title>
+          <v-card-subtitle>Supports public and server-side env variables</v-card-subtitle>
+          <v-card-text>This is an environement: <b>{{ runtimeConfig.public.apiUrl }}</b></v-card-text>
+        </base-card>
       </v-col>
 
-      <v-col cols="11" md="6">
-        <BaseCard>
-          <v-card-title>Env Demo</v-card-title>
-          <v-card-subtitle>See how the environmental variables works</v-card-subtitle>
+      <v-col v-for="feature in features" cols="11" md="6">
+        <base-card>
+          <v-card-title>{{ feature.label }}</v-card-title>
+          <v-card-subtitle>{{ feature.description }}</v-card-subtitle>
 
-          <v-card-text>
-            <ul>
-              <li>Server: {{ runtimeConfig.restApi }}</li>
-              <li>Client: {{ runtimeConfig.public.restApi }}</li>
-            </ul>
-          </v-card-text>
-        </BaseCard>
+          <v-card-actions>
+            <base-btn color="primary" :to="feature.to" @click="feature.action">
+              {{ feature.label }}
+            </base-btn>
+          </v-card-actions>
+        </base-card>
       </v-col>
     </v-row>
-  </BasePage>
+  </base-page>
 </template>
