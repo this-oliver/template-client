d25a0c2f9f9e9a9b62e67a51384433a8c1e7a262
Oliver Manzi <olivermanzi.dev@gmail.com>
2023-11-09 20:27:32 +0100

    (v0.6.0) feature: adds an example form page

---
+++ pages/form.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <base-page title="Forms">
+    <v-row justify="center">
+      <v-col cols="12" md="8">
+        <h3>Text Input</h3>
+        <InputText label="Enter text" place-holder="This is a placeholder" />
+      </v-col>
+
+      <v-col cols="12" md="8">
+        <h3>Password Input</h3>
+        <InputText label="Enter password" place-holder="This is a placeholder" type="password" />
+      </v-col>
+
+      <v-col cols="12" md="8">
+        <h3>Larger Text Input</h3>
+        <InputTextArea label="Enter more text" place-holder="Another placeholder" />
+      </v-col>
+
+      <v-col cols="12" md="8">
+        <h3>Date Input</h3>
+        <InputDateTime label="Enter a date" />
+      </v-col>
+
+      <v-col cols="12" md="8">
+        <h3>Upload Files</h3>
+        <InputFile label="Upload a file" />
+      </v-col>
+    </v-row>
+  </base-page>
+</template>
